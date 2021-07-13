8d0d25c9fb08129f0f3606ddbcfc3c11683fb78d
shirajuki <shirajuki00@gmail.com>
2021-07-13 12:25:36 +0200

    [part2] finishes 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const CountryDisplay = ({ country }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {country.languages.map((language) => (
+          <li key={language.name}>{language.name}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flag}
+        width="100px"
+        height="100px"
+        alt={`flag of ${country.name}`}
+      />
     </div>
   );
-}
+};
+
+const CountriesDisplay = ({ filteredCountries }) => {
+  if (filteredCountries.length === 1)
+    return <CountryDisplay country={filteredCountries[0]} />;
+  return filteredCountries.length > 10 ? (
+    <p>Too many matches, specify another filter</p>
+  ) : (
+    <>
+      {filteredCountries.map((country) => (
+        <p key={country.name}>{country.name}</p>
+      ))}
+    </>
+  );
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div>
+        find countries{" "}
+        <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value.toLowerCase())}
+        />
+      </div>
+      <CountriesDisplay
+        filteredCountries={
+          filter === ""
+            ? []
+            : countries.filter((country) =>
+                country.name.toLowerCase().includes(filter)
+              )
+        }
+      />
+    </div>
+  );
+};
 
 export default App;

f626090f9b536b8bf3075e4ad56aca69b12e09da
shirajuki <shirajuki00@gmail.com>
2021-07-20 13:56:42 +0200

    [part8] finishes 8.8-8.12

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+import { useQuery, useMutation } from "@apollo/client";
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  if (!props.show) return null;
+  if (result.loading) return <div>loading...</div>;
+  const authors = result.data.allAuthors || [];
+  const submit = (event) => {
+    event.preventDefault();
+    editAuthor({ variables: { name, born: Number(born) } });
+
+    setName("");
+    setBorn("");
+  };
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map((a) => (
+              <option value={a.name}>{a.name}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  );
+};
+
+export default Authors;
---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,57 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      id
+      name
+      born
+      bookCount
+    }
+  }
+`;
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      id
+      title
+      author
+      published
+      genres
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+    }
+  }
+`;
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      born
+    }
+  }
+`;
+/*
+export const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    // ...
+  }
+`;
+*/
